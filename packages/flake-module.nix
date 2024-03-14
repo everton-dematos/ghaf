db5cf6554e31736d191e885886c5a80242443f14
Manuel Bluhm <manuel@ssrc.tii.ae>
2024-03-14 18:37:52 +0400

    Changes: - updated to latest - adjusted tetragon, now running in all VMs - added admivm but not included
    Signed-off-by: Manuel Bluhm <manuel@ssrc.tii.ae>

--- packages/flake-module.nix
+++ packages/flake-module.nix
@@ -27,6 +27,7 @@
 
       #fix these to be the correct packages placement
       packages = self.lib.platformPkgs system {
+        tetragon = callPackage ./tetragon {};
         doc = callPackage ../docs {
           revision = lib.strings.fileContents ../.version;
           options =
