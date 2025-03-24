4b25f6a5f16b3d52f196775291d05252486f59d1
Everton de Matos <everton.dematos@tii.ae>
2025-03-24 16:32:54 +0400

    Basic Tetragon testing
    Signed-off-by: Everton de Matos <everton.dematos@tii.ae>

--- modules/common/security/tetragon.nix
+++ modules/common/security/tetragon.nix
@@ -16,41 +16,25 @@
     systemd.services.tetragon = {
       description = "Tetragon - eBPF Security Observability";
       wantedBy = [ "multi-user.target" ];
-      after = [ "network.target" ];
+      after = ["network.target" "local-fs.target"];
       serviceConfig = {
-        ExecStartPre = ''
-          mkdir -p /tmp/tetragon
-          mount -t bpf none /tmp/tetragon
-          mkdir -p /tmp/tetragon/tetragon
-          mount --bind /tmp/tetragon/tetragon /sys/fs/bpf/tetragon
-          chmod 700 /tmp/tetragon/tetragon
-          chown root:root /tmp/tetragon/tetragon
-        '';
-        ExecStart = "${pkgs.tetragon}/bin/tetragon --config=/etc/tetragon/config.yaml --bpf-lib=/tmp/tetragon";
-        Restart = "always";
-        RestartSec = 5;
-
-        ProtectSystem = "false";
-        ProtectHome = "false";
-        CapabilityBoundingSet = "CAP_SYS_ADMIN CAP_BPF CAP_NET_ADMIN CAP_NET_RAW";
-        AmbientCapabilities = "CAP_BPF CAP_SYS_ADMIN CAP_NET_ADMIN";
-        SystemCallFilter = [ "@ebpf" "@privileged" ];
-        SystemCallErrorNumber = "EPERM";
-
-        MountAPIVFS = true;  
-        ProtectKernelModules = false;
-        ProtectKernelLogs = false;
-        ProtectControlGroups = false;
-        ProtectKernelTunables = false;
-        ProtectProc = "noaccess";
-
-        MountFlags = "shared";
-        ReadWritePaths = [ "/sys/fs/bpf" "/sys/kernel/btf" ];
-        DeviceAllow = [ "/dev/bpf rw" ];
-
-        RestrictNamespaces = false;
-        NoNewPrivileges = false;
+        Type = "simple";
+        User = "root";
+        Group = "root";
+        Environment = "PATH=${pkgs.tetragon}/lib/tetragon/:${pkgs.tetragon}/lib:${pkgs.tetragon}/bin";
+        ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p /etc/tetragon/policies";
+        ExecStart = "${pkgs.tetragon}/bin/tetragon \
+          --config-dir=/etc/tetragon \
+          --bpf-lib ${pkgs.tetragon}/lib/tetragon/bpf \
+          --tracing-policy-dir=/etc/tetragon/policies \
+          --server-address 0.0.0.0:3333";
+        StartLimitBurst = 10;
+        StartLimitIntervalSec = 120;
       };
+    };
+
+    networking = {
+      firewall.allowedTCPPorts = [3333 2112 5555];
     };
 
     # Default Tetragon Configuration
@@ -63,46 +47,25 @@
       enforcement: true
     '';
 
-    # Security Policies
-    environment.etc."tetragon/policies/ssh-monitoring.yaml".text = ''
-      apiVersion: cilium.io/v1alpha1
-      kind: TracingPolicy
-      metadata:
-        name: ssh-monitoring
-      spec:
-        kprobes:
-        - call: "execve"
-          syscall: true
-          args:
-          - index: 0
-            type: "string"
-          selectors:
-          - matchBinaries:
-            - operator: "In"
-              values:
-              - "/usr/sbin/sshd"
-          action: "log"
-    '';
-
-    environment.etc."tetragon/policies/block-netcat.yaml".text = ''
-      apiVersion: cilium.io/v1alpha1
-      kind: TracingPolicy
-      metadata:
-        name: block-netcat
-      spec:
-          kprobes:
-          - call: "execve"
-            syscall: true
-            args:
-            - index: 0
-              type: "string"
-            selectors:
-            - matchBinaries:
-              - operator: "In"
-                values:
-                - "/usr/bin/nc"
-                - "/bin/netcat"
-            action: "deny"
-    '';
+    # environment.etc."tetragon/policies/test-ls.yaml".text = ''
+    #   apiVersion: cilium.io/v1alpha1
+    #   kind: TracingPolicy
+    #   metadata:
+    #     name: log-ls-exec
+    #   spec:
+    #     kprobes:
+    #     - call: "execve"
+    #       syscall: true
+    #       args:
+    #       - index: 0
+    #         type: "string"
+    #       selectors:
+    #       - matchBinaries:
+    #           operator: "In"
+    #           values:
+    #             - "/usr/bin/ls"
+    #             - "/bin/ls"
+    #       action: "log"
+    # '';
   };
 }
